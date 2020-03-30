91e8a455084e6bcbbd4514670e42e3ac4b27d226
YutoMizutani <yuto.mizutani.dev@gmail.com>
2020-03-30 12:19:31 +0900

    Initial Commit

---
+++ Wishwipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wishwipe
+//
+//  Created by Yuto Mizutani on 2020/03/30.
+//  Copyright © 2020 Yuto Mizutani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
